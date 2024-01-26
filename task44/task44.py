d9a1e85976ed5f26521ece0d82851811e84f2b39
Mersinres <igor.mmcolor@gmail.com>
2024-01-26 18:37:24 +0300

    + таблица csv

--- task44/task44.py
+++ task44/task44.py
@@ -12,5 +12,12 @@ import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
+
 data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot_group'] = ' 1'
+data.loc[data['whoAmI'] != 'robot', 'robot_group'] = ' 0'
+data.loc[data['whoAmI'] == 'human', 'human_group'] = ' 1'
+data.loc[data['whoAmI'] != 'human', 'human_group'] = ' 0'
 data.head()
+data.to_csv("table.csv")
+print(data)
